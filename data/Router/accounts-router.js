69ceab0ced2710656120acf8360e3805a155803d
unknown <m_fattih@hotmail.com>
2020-05-20 18:49:26 -0700

    Queries + Stretch

--- data/Router/accounts-router.js
+++ data/Router/accounts-router.js
@@ -1,98 +1,88 @@
-const express = require('express');
-const knex = require('../dbConfig.js');
-
-
+const express = require("express");
+const knex = require("../dbConfig.js");
 
 const router = express.Router();
 
-
-router.use((req,res,next) => {
-    console.log('Accounts Router Used')
-    next();
-})
-
-
-//do we need to send status we didint do on guided proj?
-router.get('/', async (req,res) => {
-
-    try{
-        const accounts = await knex('accounts')
-        res.json(accounts)  
-    }catch (err){
-        //  next({status:404, message:"biseler"})
-        res.status(500).json({errorMessage: "Server Error", error:err})
-    }
+router.use((req, res, next) => {
+  console.log("Accounts Router Used");
+  next();
+});
 
 
-})
+router.get("/", async (req, res) => {
 
+  ////// Optional Query
 
-router.get('/:id', async (req,res) => {
-    const {id} = req.params ;
+ const query = {limit:(req.query.limit ? req.query.limit : null ) , sortby:(req.query.sortby ? req.query.sortby : 'id' ) , 
+ sortdir: (req.query.sortdir  ?req.query.sortdir  : 'asc' )} 
 
-   
-    try{
-        const [account] = await knex('accounts').where({id});
-        res.json(account)  
-    }catch(err){
-        res.status(500).json({errorMessage: "Server Error", error:err})
-    }
-    
-
-
-    // knex('accounts').where({id})
-    // .then(account => {
-    //  res.status(200).json(account);
-    // }).catch(err => {
-    //     res.status(500).json({errorMessage: "Server Error", error:err})
-    // })
-
-})
-
-router.post('/', (req,res) => {
-   
-    knex('accounts').insert(req.body)
-    .then(account => {
-        res.json(req.body)
-    }).catch(err => {
-        res.status(500).json({errorMessage: "Server Error", error:err})
-    })
-
-  })
-
-
-  router.put('/:id' , async (req,res) => {
+  try {
   
-    const {id} = req.params ;
+    const accounts =  await knex.select('*').table('accounts').orderBy(query.sortby,query.sortdir).limit(parseInt(query.limit))
+    
    
-        
-            try{
-                await   knex('accounts').update(req.body).where({id})
-                res.status(200).json(req.body)
-            }catch(err){
-                res.status(500).json({errorMessage: "Server Error", error:err})
-            }
+    res.json(accounts);
+  } catch (err) {
+    
+    res.status(500).json({ errorMessage: "Server Error", error: err });
+  }
+});
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-})
+  try {
+    const [account] = await knex("accounts").where({ id });
+    res.json(account);
+  } catch (err) {
+    res.status(500).json({ errorMessage: "Server Error", error: err });
+  }
 
+  // knex('accounts').where({id})
+  // .then(account => {
+  //  res.status(200).json(account);
+  // }).catch(err => {
+  //     res.status(500).json({errorMessage: "Server Error", error:err})
+  // })
+});
 
-router.delete('/:id', async (req,res) => {
-    const {id} = req.params ;
-    try{
-        const [deleted] =  await knex('accounts').where({id});
-        const count = await knex('accounts').del().where({id});
-      if (count > 0) {
-     
-        res.status(200).json(deleted )
+router.post("/", (req, res) => {
+  knex("accounts")
+    .insert(req.body)
+    .then((account) => {
+      res.json(req.body);
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "Server Error", error: err });
+    });
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await knex("accounts").update(req.body).where({ id });
+    res.status(200).json(req.body);
+  } catch (err) {
+    res.status(500).json({ errorMessage: "Server Error", error: err });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [deleted] = await knex("accounts").where({ id });
+    const count = await knex("accounts").del().where({ id });
+    if (count > 0) {
+      res.status(200).json(deleted);
     } else {
-        res.status(404).json({ message: 'invalid id' });
+      res.status(404).json({ message: "invalid id" });
     }
-        
-    }catch(err){
-        res.status(500).json({errorMessage: "Server Error", error:err})
-    }
-})
+  } catch (err) {
+    res.status(500).json({ errorMessage: "Server Error", error: err });
+  }
+});
+
 
 
 
